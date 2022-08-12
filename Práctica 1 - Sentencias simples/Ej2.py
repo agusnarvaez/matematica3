02176fda5e84f06aed10502666a3d66c42c9254d
Narvaez <agustin.narvaez@siemens.com>
2022-08-12 19:23:58 -0300

    Práctica 1 Sentencias simples - Ejercicio 2

---
+++ Práctica 1 - Sentencias simples/Ej2.py
@@ -0,0 +1,72 @@
+""" 
+    2 - Evaluar siguientes expresiones
+    a) 1/2/4
+    b) 1/2.0/4.0
+    c) 1/2.0/4
+    d) 1.0/2/4
+    e) 4** .5
+    f) 4.0** (1 / 2)
+    g) 4.0** (1 / 2) + 1 / 2
+    h) 4.0** (1.0 / 2) + 1 / 20
+    i) 3e3 /10
+    j) 10 / 5e-3
+    k) 10 / 5e-3 + 1
+    l) 3 / 2 + 1
+"""
+#Definición de función main
+def main():
+    # a) 1/2/4
+    expresion = 1/2/4
+    
+    print(f"a) 1/2/4 = {expresion}")
+
+    # b) 1/2.0/4.0
+    expresion = 1/2.0/4.0
+    
+    print(f"b) 1/2.0/4.0 = {expresion}")
+
+    # c) 1/2.0/4
+    expresion= 1/2.0/4
+    print(f"c) 1/2.0/4 = {expresion}")
+
+    # d) 1.0/2/4
+    expresion= 1.0/2/4
+    print(f"d) 1.0/2/4 = {expresion}")
+
+    # e) 4** .5
+    expresion= 4** .5
+    print(f"e) 4** .5 = {expresion}")
+
+    # f) 4.0** (1 / 2)
+    expresion= 4.0** (1 / 2)
+    print(f"f) 4.0** (1 / 2) = {expresion}")
+
+    # g) 4.0** (1 / 2) + 1 / 2
+    expresion= 4.0** (1 / 2) + 1 / 2
+    print(f"g) 4.0** (1 / 2) + 1 / 2 = {expresion}")
+
+    # h) 4.0** (1.0 / 2) + 1 / 20
+    expresion = 4.0** (1.0 / 2) + 1 / 20
+    print(f"h) 4.0** (1.0 / 2) + 1 / 20 = {expresion}")
+
+    # i) 3e3 / 10
+    expresion = 3e3 / 10
+    print(f"i) 3e3 / 10 = {expresion}")
+
+    # j) 10 / 5e-3
+    expresion = 10 / 5e-3
+    print(f"j) 10 / 5e-3 = {expresion}")
+
+    # k) 10 / 5e-3 + 1
+    expresion = 10 / 5e-3 + 1
+    print(f"k) 10 / 5e-3 + 1 = {expresion}")
+
+    # l) 3 / 2 + 1
+    expresion = 3 / 2 + 1
+    print(f"l) 3 / 2 + 1 = {expresion}")
+
+    print("Conclusiones: Cuando ingreso un dato flotante, el resultado de las operaciones que se realicen con el mismo serán de este tipo")
+    
+
+if __name__ == "__main__":
+    main()
