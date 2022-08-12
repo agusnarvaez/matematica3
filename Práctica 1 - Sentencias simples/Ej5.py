c657adc7a6c858d3abe0b06d04ac62819136a804
agusnarvaez <agus.narvaez@outlook.com>
2022-08-12 20:33:01 -0300

    Modifiqué ej5

--- Práctica 1 - Sentencias simples/Ej5.py
+++ Práctica 1 - Sentencias simples/Ej5.py
@@ -35,7 +35,7 @@ def main():
     print(f"El doble producto del primero menos la mitad del segundo es: {resultado}")
 
     print()
-    print("Fin de programa")
+    print("Fin de programa!")
 
 if __name__ == "__main__":
     main()
