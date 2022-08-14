0173bee4d31f4ab29e760ccd337dc0de817cde69
agusnarvaez <agus.narvaez@outlook.com>
2022-08-14 12:53:49 -0300

    Ejercicio 3 Rangos

---
+++ Práctica 2 - Lista Rangos Ciclos/Rangos_Ej3.py
@@ -0,0 +1,24 @@
+"""
+    3.  Escribe un programa que pida un número entero y emita una lista de números consecutivos del 0 al
+    valor dado.
+"""
+
+#Definición de función main
+def main():
+    print("Ejercicio 1 - Listas Rangos Ciclos")
+    print()
+
+    finRango = int(input("Introduzca un número: "))
+
+    # Range crea un rango, range(inicio, fin, paso)
+    rango=range(0,finRango)
+
+    # Con list(rango) lo pasamos a formato lista
+    print(list(rango))
+
+    print()
+    print("Fin de programa")
+    
+
+if __name__ == "__main__":
+    main()
