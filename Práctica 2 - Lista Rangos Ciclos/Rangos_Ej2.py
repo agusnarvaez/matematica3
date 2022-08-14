8b452111d266cb387c6242327107429f6c634b5f
agusnarvaez <agus.narvaez@outlook.com>
2022-08-14 12:24:56 -0300

    Ejercicio 2 Rangos

--- Práctica 2 - Lista Rangos Ciclos/Rangos_Ej2.py
+++ Práctica 2 - Lista Rangos Ciclos/Rangos_Ej2.py
@@ -1,5 +1,5 @@
 """
-    1.  Escribe un tipo range() que emita: [100, 101, 102, 103]
+    2.    Escribe un tipo range() que emita: [-50, -2050, -4050, -6050]
 """
 
 #Definición de función main
@@ -8,7 +8,7 @@ def main():
     print()
 
     # Range crea un rango, range(inicio, fin, paso)
-    rango=range(100,103)
+    rango=range(-50,-6100,-2000)
 
     # Con list(rango) lo pasamos a formato lista
     print(list(rango))
